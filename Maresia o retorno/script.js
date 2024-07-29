a98f3ad5ae7f860966e4708a3649270e6cd16b03
isoyaque <166941921+isoyaque@users.noreply.github.com>
2024-07-29 10:54:50 -0300

    Add files via upload

---
+++ Maresia o retorno/script.js
@@ -0,0 +1,41 @@
+function abrir(divId) {
+    let descricoes = document.querySelectorAll('.descricao');
+    descricoes.forEach(function (descricao) {
+        if (descricao.id !== divId) {
+            descricao.classList.remove('mostrar', 'anime');
+        }
+    });
+
+    let descricao = document.getElementById(divId);
+    if (!descricao.classList.contains('mostrar')) {
+        descricao.classList.add('mostrar');
+        setTimeout(() => descricao.classList.add('anime'), 10); // Pequeno atraso para garantir a transição
+    } else {
+        descricao.classList.remove('anime');
+        setTimeout(() => descricao.classList.remove('mostrar'), 1000); // Tempo de transição de 1 segundo
+    }
+}
+
+function mostrarAdega() {
+    let adega = document.getElementById("adega");
+    let distribuidora = document.getElementById("distribuidora");
+
+    if (adega.style.display === "none" || adega.style.display === "") {
+        adega.style.display = "block";
+        distribuidora.style.display = "none";
+    } else {
+        adega.style.display = "none";
+    }
+}
+
+function mostrarDistribuidora() {
+    let adega = document.getElementById("adega");
+    let distribuidora = document.getElementById("distribuidora");
+
+    if (distribuidora.style.display === "none" || distribuidora.style.display === "") {
+        distribuidora.style.display = "block";
+        adega.style.display = "none";
+    } else {
+        distribuidora.style.display = "none";
+    }
+}
